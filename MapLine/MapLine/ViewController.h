ce7b66f35744ca1be5e09b885e383e9c3a48dc81
emmanuelgratuze <emmanuel.gratuze@gmail.com>
2012-04-05 15:30:37 +0200

    Initial commit

---
+++ MapLine/MapLine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapLine
+//
+//  Created by Emmanuel on 05/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MapLine/MapLine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapLine
+//
+//  Created by Emmanuel on 05/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
